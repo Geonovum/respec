b92fe0d0c8dfcfb08f03140cf6c4d1b531550461
dhvenema <d.h.venema@outlook.com>
2017-05-04 11:54:17 +0200

    Update d3figures.js

--- js/geonovum/d3figures.js
+++ js/geonovum/d3figures.js
@@ -8,62 +8,31 @@ define(
    */
   return {
     run: function(conf, doc, cb) {
-      var figures = $("figure.scalable");
-      
-      for (i = 0; i < figures.length; i++) {
-        var width, height, src;
-        var img = figures[i].getElementsByTagName("img")[0];
-        
-        /* Get and/or set image height */
-        if (img.getAttribute("height")) {
-          height = img.getAttribute("height");
-        } else {
-          height = 600;
-          img.setAttribute("height", height);
-        };
-        
-        /* Get and/or set image width */
-        if (img.getAttribute("width")) {
-          width = img.getAttribute("width");
-        } else {
-          width = "100%";
-          img.setAttribute("width", width);
-        };
-        
-        /* Check id */
-        if (img.getAttribute("id")) {
-          var id = img.getAttribute("id");
-        };
-        
-        /* Check src */
-        if (img.getAttribute("src")) {
-          var src = img.getAttribute("src");
-        };
-        
-        /* Check alt */
-        if (img.getAttribute("alt")) {
-          var alt = img.getAttribute("alt");
-        }
-        
-        /* Create svg element */
-        var node = document.createElement("svg")
-        svg = img.parentElement.insertBefore(node, img)
-        img.parentElement.removeChild(img)
-        
-        var svg = d3.select(svg).call(d3.zoom().on("zoom", function () {
-                      svg.attr("transform", d3.event.transform)
-                    }))
-                    .attr("width", width)
-                    .attr("height", height)
-                    .style("border", "1px solid black")
-                    .append("g");
-        
-        svg.append("img")
-           .attr("src", src)
-           .attr("id", id)
-           .attr("alt", alt);
-      };
+       Array
+        .from(doc.querySelectorAll("figure.scalable img"))
+        .forEach(function(image){
+          svg = d3.select(image.parentElement)
+                  .insert("svg", ":first-child")
+                  .attr("width", ((image.getAttribute("width")) ? image.getAttribute("width") : "100%"))
+                  .attr("height", ((image.getAttribute("height")) ? image.getAttribute("height") : 600))
+                  .style("border", "1px solid black");
+          g = svg.append("g")
+                 .attr("transform", "translate([-0, 0])scale(1)")
+                 .call(d3.zoom()
+                   .scaleExtent([1, 6])
+                   .on("zoom", function () {
+                   g.attr("transform", d3.event.transform)
+                 }))
+                 .append("g");
+          g.append("image")
+           .attr("width",  ((image.getAttribute("width")) ? image.getAttribute("width") : "100%"))
+           .attr("height", ((image.getAttribute("height")) ? image.getAttribute("height") : 600))
+           .attr("id", ((image.getAttribute("id")) ? image.getAttribute("id") : ""))
+           .attr("alt", ((image.getAttribute("alt")) ? image.getAttribute("alt") : ""))
+           .attr("src", image.getAttribute("src"))
+           .attr("xlink:href", image.getAttribute("src"));
+          image.remove();
+        });
       cb();
-    }
-  }; 
-});
+  }};
+});       
