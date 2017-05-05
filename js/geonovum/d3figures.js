14c924c0d2ffb4aecce0d28150bf9968ccc0f658
dhvenema <d.h.venema@outlook.com>
2017-05-05 13:32:11 +0200

    Update d3figures.js

--- js/geonovum/d3figures.js
+++ js/geonovum/d3figures.js
@@ -4,53 +4,61 @@ define(
   /**
    * Makes figures scalable via zoom and pan function
    *
-   */ 
-  
-  //function reset() {
-  //  svg.transition()
-  //     .duration(750)
-  //     .attr("transform", "translate(0, 0)scale(1)")
-  //     .call(d3.zoom());
-  //};
-
+   */
   return {
     run: function(conf, doc, cb) {
       Array
         .from(doc.querySelectorAll("figure.scalable img"))
-        .forEach(function(image){
-          console.log(image, image.width, image.height);
-          var svg = d3.select(image.parentElement)
-                      .insert("svg", ":first-child")
-                      .attr("id", (image.id ? image.id : ""))
-                      .attr("alt", (image.alt ? image.alt : ""));
+        .forEach(function(image) {
+          var width = image.width,
+            height = image.height;
+          var zoom = d3.zoom()
+            .scaleExtent([1, 6])
+            .on("zoom", function () {
+                          g.attr("transform", 
+                          d3.event.transform);
+                        });
+          
+          var container = d3.select(image.parentElement)
+          var svg = container.insert("svg", ":first-child")
+            .attr("height", height)
+            .attr("width", width)
+            .attr("id", (image.id ? image.id : ""))
+            .attr("alt", (image.alt ? image.alt : ""));
           var g = svg.append("g")
-                     .attr("transform", "translate(0, 0)scale(1)")
-                     .call(d3.zoom()
-                       .scaleExtent([1, 6])
-                       .on("zoom", function () {
-                       g.attr("transform", d3.event.transform)
-                     }))
-                     .append("g");
+            .call(zoom)
+            .append("g");
           g.append("image")
-           .attr("height", ((image.height < 600) ? image.height : image.height = 600))
-           .attr("width", ((image.width) ? image.width : image.width = "auto"))
-           .attr("src", image.src)
-           .attr("xlink:href", image.src);
-           
+            .attr("height", height)
+            .attr("width", width)
+            .attr("src", image.src)
+            .attr("xlink:href", image.src);
+          
           var reset_button = d3.select(image.parentElement)
-                               .insert("button", ":nth-child(2)")
-                               .attr("class", "")
-                               .attr("type", "button")
-                               .text("Reset");
-          //d3.select(reset_button).on("click", reset);
-          var enlarge_button = d3.select(image.parentElement)
-                                 .insert("button", ":nth-child(2)")
-                                 .attr("class", "")
-                                 .attr("type", "button")
-                                 .text("Enlarge");
-          d3.select(enlarge_button).on("click", window.open(image.src));
+            .insert("button", ":nth-child(2)")
+            .attr("id", "reset_button")
+            .attr("class", "button")
+            .text("Reset");
+          reset_button.on("click", function () {
+                                     svg.transition()
+                                       .duration(750)
+                                       .call(zoom.transform, 
+                                        d3.zoomIdentity);
+                                   });
+          
+          //var enlarge_button = d3.select(image.parentElement)
+          //  .insert("button", ":nth-child(2)")
+          //  .attr("id", "enlarge_button")
+          //  .attr("class", "button")
+          //  .text("Enlarge");
+          //enlarge_button.on("click", openNewTab(image.src));
           
           image.remove();
+          
+          //function openNewTab(url) {
+          //  var win = window.open(url, '_blank');
+          //  win.focus();
+          //}
         });
       cb();
     }};
