a5aa6171b40c4242edfa58fd31ee294636db3686
dhvenema <DVenema@geonovum.nl>
2017-06-08 15:13:08 +0200

    fix leafletfigures.js

--- js/geonovum/leafletfigures.js
+++ js/geonovum/leafletfigures.js
@@ -16,23 +16,31 @@ define(
           div.id = "leaflet-figure";
           container.replaceChild(div, image);
           
-          console.log(image.width, image.height);
-          
           var map = L.map(div, {
             maxZoom: 8,
             minZoom: -8,
+            center: [0, 0],
             crs: L.CRS.Simple
           }).setView([image.height/2, image.width/2], 0);
-          
-          var imageUrl = image.src;
+         
+          //var map = L.map(div, {
+          //  minZoom: 1,
+          //  maxZoom: 4,
+          //  //center: [0, 0],
+          //  zoom: 2,
+          //  crs: L.CRS.Simple
+          //});
           
           var imageBounds = [
             [0, 0],
             [image.height, image.width],
           ];
           
-          L.imageOverlay(imageUrl, imageBounds).addTo(map);
+          //var northEast = map.unproject([image.height, image.width], map.getMaxZoom()-1);
+          //var southWest = map.unproject([0, 0], map.getMaxZoom()-1);
+          //var imageBounds = new L.LatLngBounds(southWest, northEast);
           
+          L.imageOverlay(image.src, imageBounds).addTo(map);
           //map.fitBounds(imageBounds);
         });
       cb();
