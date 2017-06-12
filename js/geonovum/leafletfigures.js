85a9f8556d9cff14a33158156552b2b5519e8383
dhvenema <DVenema@geonovum.nl>
2017-06-12 19:53:45 +0200

    update leaflet figures

--- js/geonovum/leafletfigures.js
+++ js/geonovum/leafletfigures.js
@@ -14,30 +14,14 @@ define(["geonovum/deps/leaflet"], function(L) {
           var div = doc.createElement("div");
           div.id = "leaflet-figure";
           container.replaceChild(div, image);
-
           var map = L.map(div, {
             maxZoom: 8,
             minZoom: -8,
             center: [0, 0],
             crs: L.CRS.Simple,
           }).setView([image.height / 2, image.width / 2], 0);
-
-          //var map = L.map(div, {
-          //  minZoom: 1,
-          //  maxZoom: 4,
-          //  //center: [0, 0],
-          //  zoom: 2,
-          //  crs: L.CRS.Simple
-          //});
-
           var imageBounds = [[0, 0], [image.height, image.width]];
-
-          //var northEast = map.unproject([image.height, image.width], map.getMaxZoom()-1);
-          //var southWest = map.unproject([0, 0], map.getMaxZoom()-1);
-          //var imageBounds = new L.LatLngBounds(southWest, northEast);
-
           L.imageOverlay(image.src, imageBounds).addTo(map);
-          //map.fitBounds(imageBounds);
         });
       });
       cb();
